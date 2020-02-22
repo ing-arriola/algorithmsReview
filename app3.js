eab408c1abca15b9b715b42969637a1ba38c49cd
ing-arriola <ingeniero.arriola@gmail.com>
2020-02-22 17:56:38 -0600

    Search pattern added

--- app3.js
+++ app3.js
@@ -16,6 +16,23 @@ switch (specialSel.value) {
 }
 
 const searchPattern=()=>{
-    console.log("i'm here")    
+    let stringWithPattern="AABAACAADAABAABA"
+    let pattern="AABA"
+    let posiciones=[]
+    for (let i = 0; i < stringWithPattern.length-pattern.length+1; i++) {
+        //if (stringWithPattern.substring(i,pattern.length+i)===pattern){
+        if(myOwnSubString(stringWithPattern,i,pattern.length+i)===pattern){
+            posiciones.push(i)
+        }
+    }
+    viewThree.innerHTML=`Los indices en donde esta el patron son: ${posiciones}`
+}
 
+const myOwnSubString=(stringArg,start,end)=>{
+    stringArg=stringArg.split("")
+    let ans=""
+    for (let i = start; i < end; i++) {
+        ans+=stringArg[i]
+    }
+    return ans
 }
